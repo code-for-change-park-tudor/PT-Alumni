4680cf8097387ba04a58c7c86b966e7cabcd61a6
AlphaGriesemer <47705026+AlphaGriesemer@users.noreply.github.com>
2019-02-16 22:26:52 -0500

    Add files via upload

---
+++ binReadMe.txt
@@ -0,0 +1,12 @@
+﻿This Text file is the readMe for the Bin file
+
+Inside of the bin file you will find the entire front end for the main page.
+This project uses the .NET framework to link JS, CSS, and HTML to create a page that helps people look up alumni and what they are up to.
+
+Inside the JS file named,"functionsForDropDownMenu" there should be "5"(for now) functions that allow for users to toggle between a "+" or "-" on the filters
+and the first function in the file is used to redirect people to ParkTudor's main website when the click on the top banner.
+
+Inside the mainPage html file all things related to the header, search-bar, filter buttons are all located
+Question for those who read this: If i want to keep the class of a button "fa fa-plus" how can I put text before the button?
+
+Finally inside the CSS file, this contains specifics to everything in the HTML file.
---
+++ functionsForMainPage.js
@@ -0,0 +1,39 @@
+﻿function parkTudorWebsite() {
+    location.href = 'https://www.parktudor.org';
+}
+function schoolDropDown() {
+    var classToggle = document.getElementById("schoolToggle").className;
+    if (classToggle === "fa fa-plus") {
+        document.getElementById("schoolToggle").className = "fa fa-minus";
+        document.getElementById("myDropdown").classList.toggle("show");
+
+        
+    }
+    else if (classToggle === "fa fa-minus") {
+        document.getElementById("schoolToggle").className = "fa fa-plus";
+        document.getElementById("myDropdown").classList.toggle("show");
+
+    }
+}
+function majorDropDown() {
+    var classToggle = document.getElementById("majorToggle").className;
+    if (classToggle === "fa fa-plus") {
+        document.getElementById("majorToggle").className = "fa fa-minus";
+        document.getElementById("myDropdownA").classList.toggle("show");
+    }
+    else if (classToggle === "fa fa-minus") {
+        document.getElementById("majorToggle").className = "fa fa-plus";
+        document.getElementById("myDropdownA").classList.toggle("show");
+    }
+}
+function stateDropDown() {
+    var classToggle = document.getElementById("stateToggle").className;
+    if (classToggle === "fa fa-plus") {
+        document.getElementById("stateToggle").className = "fa fa-minus";
+        document.getElementById("myDropdownB").classList.toggle("show");
+    }
+    else if (classToggle === "fa fa-minus") {
+        document.getElementById("stateToggle").className = "fa fa-plus";
+        document.getElementById("myDropdownB").classList.toggle("show");
+    }
+}
